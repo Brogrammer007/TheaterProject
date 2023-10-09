243e538c9722bc6c254e6bb8fc7e466b8ecf13ec
Master Nikola <nikola_dev1@proton.me>
2023-10-10 00:28:11 +0200

    Dzoni prvi komit

---
+++ src/Predstava.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Predstava {
+    String naziv;
+    TipPredstave tipPredstave;
+    String reziser;
+    ArrayList<String> glumci;
+    int trajanjePredstave;
+    String produkcija;
+    int godinaPremijere;
+    String opisPredstave;
+
+
+
+}
---
+++ src/TipPredstave.java
@@ -0,0 +1,5 @@
+public enum TipPredstave {
+    DRAMA,
+    OPERA,
+    BALET;
+}
