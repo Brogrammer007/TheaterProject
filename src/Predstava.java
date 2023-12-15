6cbf82d67c7a55ed900d6938a018fbac5ab63cc8
Nik <nikola_dev1@proton.me>
2023-12-15 18:19:56 +0100

    Geteri i seteri na main grani

--- src/Predstava.java
+++ src/Predstava.java
@@ -10,6 +10,9 @@ public class Predstava {
     private int godina;
     private String opis;
 
+
+
+
     public Predstava(String naziv, TipPredstave tip, String reziser, ArrayList<String> glumci, int trajanje, String produkcija, int godina, String opis){
         this.naziv = naziv;
         this.TipPredstave = tip;
@@ -21,6 +24,68 @@ public class Predstava {
         this.opis = opis;
     }
 
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public TipPredstave getTipPredstave() {
+        return TipPredstave;
+    }
+
+    public String getReziser() {
+        return reziser;
+    }
+
+    public ArrayList<String> getGlumci() {
+        return glumci;
+    }
+
+    public int getTrajanje() {
+        return trajanje;
+    }
+
+    public String getProdukcija() {
+        return produkcija;
+    }
+
+    public int getGodina() {
+        return godina;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
 
 
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setTipPredstave(TipPredstave tipPredstave) {
+        TipPredstave = tipPredstave;
+    }
+
+    public void setReziser(String reziser) {
+        this.reziser = reziser;
+    }
+
+    public void setGlumci(ArrayList<String> glumci) {
+        this.glumci = glumci;
+    }
+
+    public void setTrajanje(int trajanje) {
+        this.trajanje = trajanje;
+    }
+
+    public void setProdukcija(String produkcija) {
+        this.produkcija = produkcija;
+    }
+
+    public void setGodina(int godina) {
+        this.godina = godina;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
 }
