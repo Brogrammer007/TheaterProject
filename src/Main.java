eabffc3cea2c0b4e1e510ced468a3647f3f66b21
Nik <nikola_dev1@proton.me>
2024-01-03 01:05:16 +0100

    Vracanje poslednjeg sava-a sa diska

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,57 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Koe");
+
+
+        String filePath = "resources/write.txt";
+        Predstava predstava1 = new Predstava(
+                "ProgramingWithUs",
+                TipPredstave.DRAMA,
+                "Dzo",
+                new ArrayList<String>(Arrays.asList("Coksi","Vuk","Dzo")),
+                300,
+                "TurboTransProdaksn",
+                2023,
+                "Mucenje");
+
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            writer.write(
+
+                    predstava1.getNaziv() + " | "+
+                    predstava1.getTipPredstave()+ " \n "+
+                    predstava1.getReziser()+ " \n "+
+                    predstava1.getGlumci()+ " \n "+
+                    predstava1.getTrajanje()+ " \n "+
+                    predstava1.getProdukcija()+ " \n "+
+                    predstava1.getGodina()+ " \n "+
+                    predstava1.getOpis()+ " \n ");
+            writer.close();}
+
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
-
-
 }
